e5acc2316651492a57c61262baf56c96927bac00
melliti <ramzi.melliti@epitech.eu>
2019-04-01 19:25:12 +0200

    [ADD] User Model

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,38 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+// Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields.' })
+    }
+
+    // Check password match
+    if (password != password2){
+        errors.push({ msg: 'Password do not match' })
+    }
+
+    // Check pswd length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password is not long enougth. At least 6 characters' })
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send('pass');
+    }
+
+})
+
 module.exports = router;
